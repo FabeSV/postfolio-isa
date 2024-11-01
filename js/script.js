a8c670fea09830817eb4bb2843bfa34a2ea7e740
Isabel <isa.svalero@gmail.com>
2024-11-01 21:56:49 +0100

    web

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
  
  
+// Selecciona todos los elementos con la clase "Activo"
+const elementos = document.querySelectorAll(".activo");
+
+elementos.forEach(elemento => {
+  elemento.addEventListener("click", () => {
+      
+      // Primero, desactiva la clase "encendido" en todos las activos
+      elementos.forEach(i => i.classList.remove("encendido"));
+      
+      // Luego, activa la clase "encendido" solo en la clase activa clicada
+      elemento.classList.add("encendido");
+  });
+});
+
+
  
 const navegacion = document.querySelector("nav");
 const botones = document.querySelectorAll(".open,.active-close");
@@ -12,15 +27,64 @@ function toggleNavegacion(){
    botones[i].addEventListener("click", toggleNavegacion);
 
  }
+
+
+
+  const sectionsLeft = document.querySelectorAll(".section-left");
+  // Crear un nuevo Intersection Observer
+  const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              entry.target.classList.add("show");
+              entry.target.classList.remove("hidden");
+          } else {
+              entry.target.classList.remove("show");
+              entry.target.classList.add("hidden");
+          }
+      });
+  }, {
+      threshold: 0.1 // Cambia el valor si necesitas que el efecto ocurra antes o después
+  });
+
+    // Observa cada sección
+     sectionsLeft.forEach(section => {
+      observer.observe(section);
+  });
+
+
+
+
+ const miniaturas = document.querySelectorAll(".gallery a");
+ const modal = document.querySelector(".modal") ;
+ const imgModal = document.querySelector(".modal img");
+ const flechas = document.querySelectorAll(".modal button");
+ let indiceImg = 0;
  
- /* está indicando que seleccionemos lo que esté en la clase fondo-video y lo guarde en la variable imagen*/
- let imagen = document.querySelector(".back-img");
- /* está indicando que seleccionemos todos los que estén en la clase enlaces-hover no solo a uno -- all y lo guarde en la variable enlaces*/
- let enlaces = document.querySelectorAll(".link-hover");
- for(let i = 0; i < enlaces.length; i++){
-     enlaces[i].addEventListener("mouseover", () => {
-         imagen.setAttribute("src",enlaces[i].getAttribute("href"));
+     miniaturas.forEach((miniatura,i) => {
+         miniatura.addEventListener("click", evento => {
+             evento.preventDefault();
+             indiceImg = i;
+             imgModal.setAttribute("src",miniatura.getAttribute("href"));
+            modal.classList.add("visible");
+             });
+         });
+ 
+     modal.addEventListener("click", () => {
+             modal.classList.remove("visible");
      });
- }
-
-
+     
+     flechas.forEach((flecha,i) => {
+     flecha.addEventListener("click", evento =>{
+         evento.stopPropagation();
+           if ( i == 0){
+             //atras
+             indiceImg = indiceImg > 0 ? indiceImg - 1 : miniaturas.length -1;
+           }else{
+            //adelante
+            indiceImg = indiceImg < miniaturas.length - 1 ? indiceImg + 1 : 0;
+           }
+           imgModal.setAttribute("src",miniaturas[indiceImg].getAttribute("href"));
+          console.log(indiceImg);
+ 
+     });
+     });
